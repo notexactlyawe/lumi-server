d5e12e728bb45a21ac9bf84be99c69584adc9f0a
Ami <ami1.b@hotmail.com>
2018-11-03 17:50:25 +0000

    For heroku deployment

--- app.py
+++ app.py
@@ -4,8 +4,36 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import redis
 from datetime import time, datetime, timedelta
+import googleapiclient.discovery
+import os
 
-app = Flask(__name__)
+
+
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+config = {
+  "web": {
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "redirect_uris": ["https://localhost:3000/oauth2callback"],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token"
+  }
+}
+
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+API_SERVICE_NAME = 'drive'
+API_VERSION = 'v2'
+
+
+
+app = flask.Flask(__name__)
+app.secret_key = "ears"
 redis_inst = redis.StrictRedis(host='localhost', port=6379, db=0)
 redis_inst.set('led_colour', 'red')
 
@@ -34,37 +62,117 @@ def change_led_colour():
 def get_led_colour():
     return redis_inst.get('led_colour')
 
-@app.route('/authorisation')
-def call_client_id():
-	# Use the client_secret.json file to identify the application requesting
-	# authorization. The client ID (from that file) and access scopes are required.
-	flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-	    'client_secret.json',
-	    scopes=['https://www.googleapis.com/auth/drive.metadata.readonly'])
 
-	# Indicate where the API server will redirect the user after the user completes
-	# the authorization flow. The redirect URI is required.
-	flow.redirect_uri = 'http://localhost:3000/redirect'
+@app.route('/test')
+def test_api_request():
+  if 'credentials' not in flask.session:
+    return flask.redirect('authorize')
 
-	# Generate URL for request to Google's OAuth 2.0 server.
-	# Use kwargs to set optional request parameters.
-	authorization_url, state = flow.authorization_url(
-	    # Enable offline access so that you can refresh an access token without
-	    # re-prompting the user for permission. Recommended for web server apps.
-	    access_type='offline',
-	    # Enable incremental authorization. Recommended as a best practice.
-	    include_granted_scopes='true')
+  # Load credentials from the session.
+  credentials = google.oauth2.credentials.Credentials(
+      **flask.session['credentials'])
 
-	return flask.redirect(authorization_url)
+  drive = googleapiclient.discovery.build(
+      API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
-@app.route('/redirect', methods=['GET', 'POST'])
-def response():
-	code = request.args.get('code')
-	print(code)
-	return "ok"
+  files = drive.files().list().execute()
+
+  # Save credentials back to session in case access token was refreshed.
+  # ACTION ITEM: In a production app, you likely want to save these
+  #              credentials in a persistent database instead.
+  flask.session['credentials'] = credentials_to_dict(credentials)
+
+  return flask.jsonify(**files)
 
 
-if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+@app.route('/authorize')
+def authorize():
+  # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+  flow = google_auth_oauthlib.flow.Flow.from_client_config(
+      config, scopes=SCOPES)
+
+  flow.redirect_uri = 'https://localhost:3000/oauth2callback'
+
+  authorization_url, state = flow.authorization_url(
+      # Enable offline access so that you can refresh an access token without
+      # re-prompting the user for permission. Recommended for web server apps.
+      access_type='offline',
+      # Enable incremental authorization. Recommended as a best practice.
+      include_granted_scopes='true')
+
+  # Store the state so the callback can verify the auth server response.
+  flask.session['state'] = state
+
+  return flask.redirect(authorization_url)
 
 
+@app.route('/oauth2callback', methods=['GET', 'POST'])
+def oauth2callback():
+  # Specify the state when creating the flow in the callback so that it can
+  # verified in the authorization server response.
+  state = flask.session['state']
+  code = request.args.get('code')
+  flow = google_auth_oauthlib.flow.Flow.from_client_config(
+    config, scopes=SCOPES, state=state)
+  flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+  # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+  authorization_response = flask.request.url
+  flow.fetch_token(authorization_response=code)
+
+  # Store credentials in the session.
+  # ACTION ITEM: In a production app, you likely want to save these
+  #              credentials in a persistent database instead.
+  credentials = flow.credentials
+  flask.session['credentials'] = credentials_to_dict(credentials)
+
+  return "ok"
+
+
+@app.route('/revoke')
+def revoke():
+  if 'credentials' not in flask.session:
+    return ('You need to <a href="/authorize">authorize</a> before ' +
+            'testing the code to revoke credentials.')
+
+  credentials = google.oauth2.credentials.Credentials(
+    **flask.session['credentials'])
+
+  revoke = requests.post('https://accounts.google.com/o/oauth2/revoke',
+      params={'token': credentials.token},
+      headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+  status_code = getattr(revoke, 'status_code')
+  if status_code == 200:
+    return('Credentials successfully revoked.' + print_index_table())
+  else:
+    return('An error occurred.' + print_index_table())
+
+
+@app.route('/clear')
+def clear_credentials():
+  if 'credentials' in flask.session:
+    del flask.session['credentials']
+  return ('Credentials have been cleared.<br><br>' +
+          print_index_table())
+
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
+
+
+
+if __name__ == '__main__':
+  # When running locally, disable OAuthlib's HTTPs verification.
+  # ACTION ITEM for developers:
+  #     When running in production *do not* leave this option enabled.
+  
+
+  # Specify a hostname and port that are set as a valid redirect URI
+  # for your API project in the Google API Console.
+  app.run(port=3000, debug=True, ssl_context='adhoc')
